0b7ddd6a31d0be249854260fd8e61b04b96b8634
Sylvain	Peigney <sylvain@volpeo.fr>
2016-08-11 18:58:28 +0200

    simple form + bootstrap + can add a cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,6 +13,18 @@ class CocktailsController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 
 end
