3410dd7dd72b9c12022e9d78abdd58548d13d7db
Sylvain	Peigney <sylvain@volpeo.fr>
2016-08-11 18:53:30 +0200

    begin controller for cocktail

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,18 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "cocktails#index"
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create, :destroy], shallow: true
   end
